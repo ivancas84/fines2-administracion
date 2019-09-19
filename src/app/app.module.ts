eb0e8e34ce00a70e270891186c4a935982c81cf5
unknown <ivancas84@gmail.com>
2019-09-18 23:33:43 -0300

    agregar data definition service al modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataDefinitionService } from 'src/core/service/data-definition/data-definition.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DataDefinitionService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
