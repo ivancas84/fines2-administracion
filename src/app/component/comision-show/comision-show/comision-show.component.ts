9d90bbedc3f1ac3177c0738fe7c61ec4e1149d2f
ivan <ivancas84@gmail.com>
2020-03-06 15:15:33 -0300

    incorporar horarios comision

--- src/app/component/comision-show/comision-show/comision-show.component.ts
+++ src/app/component/comision-show/comision-show/comision-show.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { ShowComponent } from '@component/show/show.component';
+import { first } from 'rxjs/operators';
+import { arrayColumn } from '@function/array-column';
+import { arrayCombineKey } from '@function/array-combine-key';
 
 @Component({
   selector: 'app-comision-show',
@@ -18,6 +21,33 @@ export class ComisionShowComponent extends ShowComponent {
   ) {
     super(dd, route, router);
   }
+
+
+  initData(){
+    if(this.mode == "reload")
+      this.getCount().pipe(first()).subscribe(
+        count => { 
+          if(this.collectionSize$.value != count) this.collectionSize$.next(count); 
+        }
+      );
+
+    this.getData().pipe(first()).subscribe(
+      rows => { 
+        var idComisiones = arrayColumn(rows, "id");
+
+        this.dd.data("horarios_comision",JSON.stringify(idComisiones)).subscribe(
+          horarios => {
+            horarios = arrayCombineKey(horarios, "comision")
+            console.log(horarios);
+          }
+        );
+        this.data$.next(rows); 
+      }
+    );
+
+    this.mode = "reload";
+  }
+
 
 }
 
