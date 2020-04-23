63d228eaf0e492d303bfbc1da8ceae1804e0ed2a
ivancas84 <ivancas84@gmail.com>
2020-04-23 11:23:27 -0300

    adaptar componente a la nueva estructura de acceso a datos de nested components e implementar nueva estructura de distribucion horaria y consultas de horarios

--- src/app/component/comision-detail/comision-detail/comision-detail.component.ts
+++ src/app/component/comision-detail/comision-detail/comision-detail.component.ts
@@ -4,10 +4,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { ToastService } from '@service/ng-bootstrap/toast.service';
 import { DetailComponent } from '@component/detail/detail.component';
-import { Observable } from 'rxjs';
-import { Display } from '@class/display';
-import { map, mergeMap } from 'rxjs/operators';
-import { arrayColumn } from '@function/array-column';
 
 @Component({
   selector: 'app-comision-detail',
@@ -26,47 +22,5 @@ export class ComisionDetailComponent extends DetailComponent {
   ) {
     super(route, router, location, dd, toast);
   }
-
-  curso_$: Observable<any>;
-  curso_sin_horario_$: Observable<any>;
-  
- 
-  
-  ngOnInit(): void {
-    var s = this.route.queryParams.subscribe (
-      params => { this.setDataFromParams(params); },
-      error => { this.toast.showDanger(JSON.stringify(error)); }
-    );
-    this.subscriptions.add(s);
-    
-    this.data$.subscribe(
-      comision => {     
-        if(comision && comision.hasOwnProperty("id") && comision["id"]) {
-          var d = new Display();
-          d.setParams({"cur_comision":comision["id"]});
-          d.setOrder({"dia_dia":"asc", "hora_inicio":"asc"});
-          this.curso_$ = this.dd.all("horario", d);
-          
-          /*this.curso_sin_horario_$ = this.curso_$.pipe(
-            mergeMap(
-              horario => {
-                var idCursos = arrayColumn(horario,  "curso");
-                var d = new Display();
-                d.setOrder({"ch_asi_nombre":"asc"});
-                d.setParams({"comision":comision["id"]});
-                if(idCursos.length) d.addCondition(["id","!=", idCursos]);
-                return this.dd.all("curso", d);
-                
-            }
-          ));*/
-
-     
-
-
-        }
-      }
-    )
-  }
-
 }
 
