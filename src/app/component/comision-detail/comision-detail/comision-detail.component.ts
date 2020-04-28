e8fe57b82de43e302aa7fddb7364a6baf4501cbb
ivancas84 <ivancas84@gmail.com>
2020-04-28 03:25:34 -0300

    Reestructurar componente comision-detail para incorporar formulario de carga de horarios

--- src/app/component/comision-detail/comision-detail/comision-detail.component.ts
+++ src/app/component/comision-detail/comision-detail/comision-detail.component.ts
@@ -3,24 +3,39 @@ import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { ToastService } from '@service/ng-bootstrap/toast.service';
-import { DetailComponent } from '@component/detail/detail.component';
+import { FormBuilder } from '@angular/forms';
+import { ValidatorsService } from '@service/validators/validators.service';
+import { SessionStorageService } from '@service/storage/session-storage.service';
+import { AdminComponent } from '@component/admin/admin.component';
 
 @Component({
   selector: 'app-comision-detail',
   templateUrl: './comision-detail.component.html',
 })
-export class ComisionDetailComponent extends DetailComponent {
+export class ComisionDetailComponent extends AdminComponent {
 
   readonly entityName: string = "comision";
 
   constructor(
-    protected route: ActivatedRoute,
-    protected router: Router,
-    protected location: Location,
-    protected dd: DataDefinitionService,
-    protected toast: ToastService,
+    protected fb: FormBuilder, 
+    protected route: ActivatedRoute, 
+    protected router: Router, 
+    protected location: Location, 
+    protected dd: DataDefinitionService, 
+    protected toast: ToastService, 
+    protected validators: ValidatorsService,
+    protected storage: SessionStorageService, 
   ) {
-    super(route, router, location, dd, toast);
+    super(fb, route, router, location, dd, toast, validators, storage);
   }
+
+  ngOnInit() {
+    var s = this.route.queryParams.subscribe (
+      params => { this.setDataFromParams(params); },
+      error => { this.toast.showDanger(JSON.stringify(error)); }
+    );
+    this.subscriptions.add(s);
+  }
+
 }
 
