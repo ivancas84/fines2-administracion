73715bb8bbff773ddfae2c584c6ccd0292844cc5
ivancas84 <ivancas84@gmail.com>
2020-05-22 17:04:22 -0300

    definir valores por defecto del formulario de busqueda

--- src/app/component/comision-show/comision-show/comision-show.component.ts
+++ src/app/component/comision-show/comision-show/comision-show.component.ts
@@ -5,6 +5,7 @@ import { ShowComponent } from '@component/show/show.component';
 import { first } from 'rxjs/operators';
 import { arrayColumn } from '@function/array-column';
 import { arrayCombineKey } from '@function/array-combine-key';
+import { Display } from '@class/display';
 
 @Component({
   selector: 'app-comision-show',
@@ -20,6 +21,16 @@ export class ComisionShowComponent extends ShowComponent {
     protected router: Router
   ) {
     super(dd, route, router);
+  }
+
+  initDisplay(params){
+    this.display = new Display();
+    this.display.setSize(100);
+    this.display.setConditionByQueryParams(params);
+    this.display.addParamIfNot("autorizada", "true");
+    this.display.addParamIfNot("fecha_anio", "2020");
+    this.condition$.next(this.display.getCondition());
+    this.params$.next(this.display.getParams());
   }
 
 
